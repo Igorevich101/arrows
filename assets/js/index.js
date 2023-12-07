8a5261e2f48c02ef281aeff4a57125261fa58f8e
igorevich101 <igorevich101@gmail.com>
2023-12-07 21:29:42 +0200

    add fixed the first problem

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,28 @@
 'use strict'
 
 // Створити функцію, яка може приймати необмежену кількість вхідних значень, та буде повертати кількість рядків, які будуть серед переданих даних. При рішенні використати стрілочні функції.
-const numberOfRows = (...restNumbers) => {
-  return restNumbers.length;
+
+
+const numberOfString = (...restNumbers) => {
+  let result = 0;
+  for ( let i = 0 ; i < restNumbers.length ; i++) {
+    typeof(restNumbers[i]) === 'string'? result++ : '';
+  }
+  console.log(result);
 }
+
+
+
+
+// const newLetter =
+// Math.random() > 0.5 ? letter.toUpperCase() : letter.toLowerCase();
+// return newLetter;
 
 
 // Створити функцію просунутого калькулятора. Функція приймає спочатку знак, а потім необмежену кількість чисел, над якими буде виконана вказана операція.
 // * 2 3 4
+
+
 const calculatorAdvances = (sign, ...restNumbers) => {
   let result = eval(restNumbers.join(sign));
   return result;
